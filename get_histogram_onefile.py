1b37714e79ded78b233f50c1eeeff47816f31420
Anna <anna.bonadeo03@gmail.com>
2025-02-28 21:48:51 +0100

    hist

---
+++ get_histogram_onefile.py
@@ -0,0 +1,15 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+file_array = input("Enter the file path: ")
+hist, bins = np.histogram(file_array, bins=65536, range=(0, 65536))
+
+plt.hist(hist, bins=bins)
+plt.figure(figsize=(10, 6))
+plt.plot(bins[:-1], hist)  # The bin edges exclude the last one
+plt.xlabel('Voxel Intensity')
+plt.ylabel('Frequency')
+plt.title('Histogram of Voxel Intensities (Rescaled)')
+plt.grid(True)
+plt.show()
