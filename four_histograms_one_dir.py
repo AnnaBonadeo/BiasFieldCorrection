caae96b9781769dc88d6eaa789ed35421a8e22e1
Anna <anna.bonadeo03@gmail.com>
2025-03-19 17:19:12 +0100

    j

--- four_histograms_one_dir.py
+++ four_histograms_one_dir.py
@@ -42,9 +42,9 @@ def calculate_tumor_histogram(mri_n4_fname: str, array_mri_n4: np.array, array_t
 
     hist_mri_n4, bins_mri_n4 = np.histogram(array_mri_n4, bins=bins_number, range=(0, 65536))
     hist_tumor_n4, bins_tumor_n4 = np.histogram(array_tumor_n4, bins=bins_number, range=(0, 65536))
-    print('Checking tumor area')
-    area_tumor = sum(np.diff(bins_tumor_n4) * hist_tumor_n4)
-    print(area_tumor)
+    print('Checking non zero voxels')
+    area_tumor = np.sum(hist_tumor_n4[hist_tumor_n4 > 0])  # Sum only non-zero voxels
+    print('Non zero voxels:' , area_tumor)
     if ax is None:  # If no axes provided, create a new plot
         fig, ax = plt.subplots()
 
