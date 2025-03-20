d468e55dafe04e2c96dbd2f58fe468be52a10754
Anna <anna.bonadeo03@gmail.com>
2025-03-20 16:24:38 +0100

    j

--- four_scatterplots_biasfield_native_image.py
+++ four_scatterplots_biasfield_native_image.py
@@ -34,7 +34,7 @@ def get_patients_number():
         except ValueError:
             print("Invalid input. Please enter a number.")
 
-def calculate_scatterplot_biasfield_native(native_mri_array:np.array,biasfield_array:np.array, mri_fname:str, display = False, save = False, ax = None):
+def calculate_scatterplot_biasfield_native(mri_fname, native_mri_array:np.array,biasfield_array:np.array,display = False, save = False, ax = None):
     shape_native = np.shape(native_mri_array)
     shape_biasfield = np.shape(biasfield_array)
     print("Native ", shape_native, "Biasfield ", shape_biasfield)
