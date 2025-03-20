835ecadaf7d8f2431c061e7c6d7e7fac465c2434
Anna <anna.bonadeo03@gmail.com>
2025-03-20 16:01:07 +0100

    j

--- four_histograms_one_dir.py
+++ four_histograms_one_dir.py
@@ -109,7 +109,7 @@ def continue_main_for_new_patient(NEW_DIR, folder_name, mri_type, patient_number
     while user_ans == 'Y':
         mri_type = get_user_answer(INPUT_MRI)
         patient_number = get_patients_number()
-        calculate_all_histograms_mri_type(NEW_DIR, f'UCSF-PDGM-0{patient_number}_nifti', mri_type, patient_number,
+        calculate_all_histograms_mri_type(NEW_DIR, f'UCSF-PDGM-{patient_number}_nifti', mri_type, patient_number,
                                           display=True, save=False)
         plt.tight_layout()
         plt.show()
--- four_scatterplots_biasfield_native_image.py
+++ four_scatterplots_biasfield_native_image.py
@@ -109,9 +109,9 @@ def continue_main_for_new_patient(NEW_DIR, folder_name, mri_type, patient_number
     if __name__ == '__main__':
         mri_type = get_user_answer(INPUT_MRI)
         patient_number = get_patients_number()
-        compute_all_scatterplots_mri_type(NEW_DIR, f'UCSF-PDGM-0{patient_number}_nifti', mri_type, patient_number,
+        compute_all_scatterplots_mri_type(NEW_DIR, f'UCSF-PDGM-{patient_number}_nifti', mri_type, patient_number,
                                           display=True, save=False)
         plt.tight_layout()
         plt.show()
-        continue_main_for_new_patient(NEW_DIR, f'UCSF-PDGM-0{patient_number}_nifti', mri_type, patient_number,
+        continue_main_for_new_patient(NEW_DIR, f'UCSF-PDGM-{patient_number}_nifti', mri_type, patient_number,
                                       display=True, save=False)
